e2396a9ee0ea6bd5d8b9f8da45ee8e9727ee00f3
lovis-ambrose <lovisambrose212@gmail.com>
2023-11-06 18:11:38 +0300

    debugged

--- server/server.py
+++ server/server.py
@@ -1,31 +1,36 @@
-from flask import Flask, request,jsonify
+from flask import Flask,request,jsonify
 import util
 
 app = Flask(__name__)
 
-
-@app.route("/get_location_names")
+# write route to get location names
+@app.route('/get_location_names')
 def get_location_names():
     response = jsonify({
-        "locations":util.get_location_names()
+        'locations': util.get_location_names()
     })
-    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Origin','*')
     return response
 
-@app.route("./predict_home_price",methods=["POST"])
+@app.route('/predict_home_price',methods = ['POST'])
 def predict_home_price():
-    total_sqft = float(request.form["total_sqft"])
-    location = request.form["location"]
-    bhk = int(request.form["bhk"])
-    bath = int(request.form["bath"])
-    # call estimated_price function
-    response = jsonify({
-        "estimated_price":util.get_estimated_price(location,total_sqft,bhk,bath)
-    })
-    response.headers.add("Access-Control-Allow-Origin","*")
-    return response
+    # return all the requests from inputs in form of regest.form
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
 
-# route to return locations
+    # now call util.get_estimated_price function
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+    })
+
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return  response
+
+
 if __name__ == "__main__":
-    print("Strating python Flask server for Home price prediction....")
-    app.run() # runs the application on a specific port
+    print("starting Python Flask server for Home Price Prediction")
+    app.run()
+
+# download and install postman for testing (used for creating and testing APIs.)
